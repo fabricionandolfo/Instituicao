44d925d4b7bb995681a71a5263fa0e85e9083bff
Aborecido <aboreciido@hotmail.com>
2020-11-03 23:19:37 -0300

    testeee

--- Projeto_Instituicao/src/projeto_instituicao/Projeto_Instituicao.java
+++ Projeto_Instituicao/src/projeto_instituicao/Projeto_Instituicao.java
@@ -15,7 +15,6 @@ public class Projeto_Instituicao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("oi");
         System.out.println("teste");
         
     }
