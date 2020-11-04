701b45dffe037d61899d9ef30aaf4eb256dda27f
fabricionandolfo <42248827+fabricionandolfo@users.noreply.github.com>
2020-11-03 23:16:15 -0300

    teste
    teste

--- Projeto_Instituicao/src/projeto_instituicao/Projeto_Instituicao.java
+++ Projeto_Instituicao/src/projeto_instituicao/Projeto_Instituicao.java
@@ -16,6 +16,7 @@ public class Projeto_Instituicao {
      */
     public static void main(String[] args) {
         System.out.println("oi");
+        System.out.println("ola");
         
     }
     
