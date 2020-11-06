bea7eb0da84b45ce8ae2b3de1aeb466a1dcc8dc1
Aborecido <aboreciido@hotmail.com>
2020-11-06 07:44:13 -0300

    teste

--- Projeto_Instituicao/src/projeto_instituicao/Projeto_Instituicao.java
+++ Projeto_Instituicao/src/projeto_instituicao/Projeto_Instituicao.java
@@ -15,13 +15,8 @@ public class Projeto_Instituicao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
-        
         System.out.println("   asodjiaosd ");
-        
-        
+
         
         
         
